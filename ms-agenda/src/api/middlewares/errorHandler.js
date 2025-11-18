14c3c6cf5795982d5c38545b2681df88312b11a1
SebsgoF11 <patinoariasleonardo@gmail.com>
2025-11-17 21:08:23 -0500

    Pregunta 1
    para solucionar el caso de la Falla de concurrencia 1ro se altero la tabla de bloqueos para que los tutores y sus horarios no se puedan reservar a la vez por distrintos estudiantes
    
    y luego se mejoro el errorHandler para que cuando encuentre el error 23505 nos devuelva el error 409 que indicara el bloqueo del tutor y su hora

--- ms-agenda/src/api/middlewares/errorHandler.js
+++ ms-agenda/src/api/middlewares/errorHandler.js
@@ -3,6 +3,15 @@
 const errorHandler = (err, req, res, next) => {
     console.error(`[ERROR] ${new Date().toISOString()}: ${err.message}`);
 
+    if (err.code === '23505') {
+        return res.status(409).json({
+            error: {
+                message: 'El tutor ya tiene una reserva en esa fecha y hora.',
+                statusCode: 409
+            }
+        });
+    }
+
     const statusCode = err.statusCode || 500;
     const errorMessage = err.statusCode ? err.message : 'Ocurrió un error inesperado en el servidor.';
 
